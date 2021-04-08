cd8b0625ae0a766e1374150686ab32ed308a70af
kimlai <hello@kimlaitrinh.me>
2021-04-08 15:52:01 +0200

    use static maps

--- lib/equiroute_web/views/page_view.ex
+++ lib/equiroute_web/views/page_view.ex
@@ -27,10 +27,6 @@ defmodule EquirouteWeb.PageView do
     "#{String.pad_leading("", padding - String.length(source.name), "-")}--->"
   end
 
-  def render("scripts.result.html", _) do
-    ~E"<script defer src='https://api.mapbox.com/mapbox-gl-js/v2.2.0/mapbox-gl.js'></script>"
-  end
-
   def render("css.result.html", _) do
     ~E"<link href='https://api.mapbox.com/mapbox-gl-js/v2.2.0/mapbox-gl.css' rel='stylesheet' />"
   end
